22f72d820b08223bd0d08959a0074ff8d603cb97
felipesan036 <77638948+felipesan036@users.noreply.github.com>
2026-08-16 23:10:44 -0300

    Create descrip.py

---
+++ descrip.py
@@ -0,0 +1,21 @@
+from cryptography.fernet import Fernet
+
+# Carrega a mesma chave
+with open("chave.key", "rb") as arquivo_chave:
+    chave = arquivo_chave.read()
+
+fernet = Fernet(chave)
+
+# Lê o arquivo criptografado
+caminho_arquivo = "empresa2026/arquivo.txt"
+with open(caminho_arquivo, "rb") as arquivo:
+    conteudo_cifrado = arquivo.read()
+
+# Descriptografa os dados
+conteudo_decriptado = fernet.decrypt(conteudo_cifrado)
+
+# Restaura o arquivo ao estado original
+with open(caminho_arquivo, "wb") as arquivo:
+    arquivo.write(conteudo_decriptado)
+
+print("Arquivo restaurado ao estado original.")
